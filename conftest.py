a7254ca229bd150d2cec7741d7a5912bf017f7d0
Nia McNichols <tmcnicho@login1.cm.cluster>
2023-08-28 20:22:05 -0700

    Add Filter base class

---
+++ conftest.py
@@ -0,0 +1,1 @@
+'''This file makes pytest use the root directory for tests'''
---
+++ tests/test_filter.py
@@ -0,0 +1,35 @@
+import pytest
+
+from vipdopt.filter import Filter
+from .utils import is_equal
+
+import numpy as np
+
+@pytest.mark.parametrize(
+    'min, max, var, expected',
+    [
+        (0, 100, 50, True),
+        (-100, 0, -50, True),
+        (0, 100, 101, False),
+        (0, 100, 100, True),
+        (0, 100, 0, True),
+        (0, 0, 1, False),
+        (0, 1, 0.5, True)
+    ]
+)
+def test_verify_bounds(min: float, max: float, var: np.ndarray, expected: bool):
+    filter = Filter((min, max))
+    result = filter.verify_bounds(var)
+
+    print(result)
+
+    assert is_equal(result, expected)
+
+def test_base_filter_not_impl():
+    filter = Filter((0, 1))
+
+    with pytest.raises(NotImplementedError):
+        filter.forward(None)
+
+    with pytest.raises(NotImplementedError):
+        filter.chain_rule(None, None, None)
