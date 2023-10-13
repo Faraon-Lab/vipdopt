606184a0962acdf72b7547d7b7ef6f361f4a79a5
Nia McNichols <tmcnicho@login1.cm.cluster>
2023-10-13 10:21:29 -0700

    Implement MPIPool

--- vipdopt/manager.py
+++ vipdopt/manager.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python3
-"""Code for managing the program's environemnt"""
-
-import sys
 import os
-import socket
-import time
+import sys
+import atexit
 import logging
-from typing import Any, Callable
+import socket
+from typing import Any, Callable, Iterable
+
 
 from mpi4py import MPI
-from mpi4py.futures import MPIPoolExecutor
 import numpy as np
 
+if __name__ == '__main__':
+    #  Here so that running this script directly doesn't blow up
+    f = sys.modules[__name__].__file__
+    if not f:
+        raise ModuleNotFoundError('SHOULD NEVER REACH HERE')
+
+    path = os.path.dirname(f)
+    path = os.path.join(path, '..')
+    sys.path.insert(0, path)
+
+from vipdopt.utils import T, R
+
+ROOT = 0
 
 SLURM_ENV_VARS = (
     'SLURM_JOB_ID',
@@ -21,152 +31,174 @@ SLURM_ENV_VARS = (
     'SLURM_JOB_NODELIST',
     'SLURM_JOB_NUM_NODES',
     'SLURM_NPROCS'
-)
-
-VALID_JOB_STATES =  ['', 'all', 'pending', 'completed']
+) 
 
 
-class Job(object):
-    """Class for subprocess-related code."""
+class MPIPool(object):
 
-    def __init__(self, name: str, f: Callable, f_args: tuple) -> None:
-        self.name = name
-        self.f = f
-        self.f_args = f_args
+    def __init__(self, comm=None) -> None:
+        if comm is None:
+            comm = MPI.COMM_WORLD
+        self.comm = comm
 
-        self.complete = False
-    
-    def __str__(self) -> str:
-        f'Job: \'{self.ame}\''
-    
-    def execute(self) -> Any:
-        res = self.f(*self.f_args)
-        self.cmplete = True
-        logging.bug(f'Completed {self}. Returned result: {res}.')
-        return res
+        self.rank = self.comm.Get_rank()
 
+        atexit.register(MPIPool.close, self)
 
-# TODO: Run it normally (python manager.py)
-# Make it create a slurm file that runs all of the jobs and asks for the 
-# appropriate amount of resources ? Then run_jobs is opening a subprocess and running
-# sbatch <slurm_filename>. For example:
-"""
-def run_jobs_inner( queue_in ):
-	processes = []
-	job_idx = 0
-	while not queue_in.empty():
-		get_job_path = queue_in.get()
-
-		logging.info(f'Node {cluster_hostnames[job_idx]} is processing file at {get_job_path}.')
-
-		process = subprocess.Popen(
-			[
-				'/home/gdrobert/Develompent/adjoint_lumerical/inverse_design/run_proc.sh',
-				cluster_hostnames[ job_idx ],
-				get_job_path
-			]
-		)
-		processes.append( process )
-"""
-# where run_proc.sh contains:
-"""/central/home/ifoo/lumerical/2021a_r22/mpich2/nemesis/bin/mpiexec -verbose -n 8 -host $1 /central/home/ifoo/lumerical/2021a_r22/bin//fdtd-engine-mpich2nem -t 1 $2 > /dev/null 2> /dev/null"""
-# ? Maybe this is what srun does already ?
-
-class WorkloadManager(object):
-    """Class for managing jobs and resources needed for parallelized code."""
-
-    def __init__(self):
-        self.__dict__.update({var:os.getenv(var) for var in SLURM_ENV_VARS})
-        
-        self.jobs: list[Job] = []
-        self.pending_jobs: list[Job] = []
-        self.completed_jobs: list [Job] = []
-    
-    def __str__(self) -> str:
-        return f'WorkloadManager with {({var:self.__getattribute__(var) for var in SLURM_ENV_VARS})}'
-    
-    def num_jobs(self, state: str='pending') -> int:
-        state = state.lower()
-        match state:
-            case '' | 'all':
-                return len(self.jobs)
-            case 'pending':
-                return len(self.pending_jobs)
-            case 'completed':
-                return len(self.completed_jobs)
-            case _:
-                raise ValueError(f'Expected state in {VALID_JOB_STATES}, got {state}.')
-
-    def add_job(self, job: Job):
-        self.jobs.append(job)
-        self.pending_jobs.append(job)
-    
-    def run_jobs(self):
-        comm_world = MPI.COMM_WORLD
-        size = comm_world.Get_size()
-        logging.debug(f'Executing {self.num_jobs()} jobs with comm_world size of {size}')
-        my_rank = comm_world.Get_rank()
-        
-        # TODO: Doesn't work for when there are more jobs than processes
-        # If we don't need this process, don't bother
-        if my_rank >= self.num_jobs():
+        if self.is_worker():
+            self.wait()
             return
-        logging.debug(f'PE{my_rank}:\tstarting job...')
         
-        my_job = self.pending_jobs[my_rank]
-        my_job.execute()
-        logging.debug(f'PE{my_rank}:\t{my_job} Complete')
+        comm_size = self.comm.Get_size()
 
-        self.clean_jobs()
-    
-    def clean_jobs(self):
-        self.completed_jobs = self.completed_jobs + \
-            [j for j in self.pending_jobs if j.complete]
-        self.pending_jobs = [j for j in self.pending_jobs if not j.complete]
-    
+        self.workers = set(range(comm_size))
+        self.workers.discard(ROOT)
+        self.size = comm_size - 1
 
-class Worker(object):
+        if self.size == 0:
+            raise ValueError("More than one process required to create MPIPool.")
 
-    def __init__(self, comm_world, rank_world, manager: WorkloadManager) -> None:
-        self.comm_world = comm_world
-        self.rank_world = rank_world
-        self.manager = manager
-    
-    def _idle(self):
-        pass
-    
+    def wait(self):
+        """If a worker, wait for work to be provided."""
+        if self.is_manager():
+            return
 
-def test_work(n, N):
-    time.sleep(2 * n / N)
-    return f'Process running on host: {socket.gethostname()}. {n}*2 = {n*2}'
+        # logging.debug(f'Worker {self.rank} waiting for work...')
+        
+        status = MPI.Status()
+        while True:
+
+            # Blocking receive
+            work = self.comm.recv(source=ROOT, tag=MPI.ANY_TAG, status=status)
+
+            if work is None:  # None is the signal to stop
+                logging.debug(f'Worker {self.rank} told to stop')
+                return
+
+            func, args = work
+
+            # logging.debug(f'Worker {self.rank} received work {work} with tag {status.tag}...')
+
+            res = func(args)
+            # logging.debug(f'Worker {self.rank} sending result {res}...')
+
+            self.comm.ssend(res, ROOT, status.tag)
+
+    def is_manager(self):
+        return self.rank == ROOT
+    
+    def is_worker(self):
+        return self.rank != ROOT
+    
+    def submit(self, func, task):
+        return self.map(func, [task])[0]
+    
+    def map(self, func: Callable[..., R], tasks: Iterable[Any]) -> Iterable[R]:
+        if self.is_worker():
+            self.wait()
+            return
+
+        available_workers = self.workers.copy()
+        jobs = [(idx, (func, arg)) for idx, arg in enumerate(tasks)]
+        logging.debug(f'Jobs: {jobs}\n')
+        results = [None] * len(jobs)
+        pending = len(jobs)
+
+        logging.debug(f'Available workers: {available_workers}\n')
+
+        while pending > 0:
+            if available_workers and len(jobs) > 0:
+                worker = available_workers.pop()  # This worker is busy now
+                idx, job = jobs.pop()
+
+                # logging.debug(f'Sending job {job} with index {idx}')
+                # logging.debug(f'Anticipated result: {job[0](job[1])}\n')
+
+                # Non-blocking send
+                self.comm.send(job, dest=worker, tag=idx)
+                logging.debug(f'Sent job {job} to worker {worker} with tag {idx}')
+            
+            if len(jobs) > 0:
+                # check if any workers are sending results
+                flag = self.comm.Iprobe(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+
+                # If there are more jobs to do and no results yet,
+                # we can keep assigning them while we wait for results
+                if not flag:
+                    continue
+            # else:
+            #     self.comm.Probe(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG)
+            
+            status = MPI.Status()
+            # Blocking receive
+            res = self.comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+
+            worker = status.source
+            idx = status.tag
+            logging.debug(f'Received result from worker {worker} with tag {idx}')
+
+            results[idx] = res
+
+            available_workers.add(worker)  # Allow worker to be assigned work
+            pending -= 1
+        
+        return results
+
+    def close(self):
+        if self.is_worker():
+            return
+        
+        # Send 'None' to each worker to signal to stop the wait loop
+        for worker in self.workers:
+            self.comm.isend(None, worker)
+        
+    def __enter__(self):
+        return self
+    
+    # TODO: May need to handle exceptions, cause it's just hanging
+    def __exit__(self, *args):
+        return self.close()
+
+
+def test_work(n):
+    # time.sleep(2 * n / N)
+    return n ** 2
+
+def test_multiproc(pow):
+    comm_world = MPI.COMM_WORLD
+    #size = comm_world.Get_size()
+    my_rank = comm_world.Get_rank()
+    data = np.array(np.power(pow, my_rank), dtype=np.intc)
+
+    sum_buf = np.empty(1, dtype=np.intc)
+
+    comm_world.allreduce(data, sum_buf)
+    return sum_buf
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-    N = int(1e2)
-
-
-    slurm_env_vars = {var:os.getenv(var) for var in SLURM_ENV_VARS}
-    print(slurm_env_vars)
-    nnodes = int(slurm_env_vars['SLURM_JOB_NUM_NODES'])
-    nprocs_per_node = int(slurm_env_vars['SLURM_CPUS_ON_NODE'])
-    nprocs = nnodes * nprocs_per_node
-    
+    N = int(1e6)
 
     comm_world = MPI.COMM_WORLD
     size = comm_world.Get_size()
     my_rank = comm_world.Get_rank()
-    print(f'MPI size:\t{size}')
+
+    if my_rank == ROOT:
+        logging.debug(f'MPI size:\t{size}')
 
 
-    if my_rank == 0:
+        slurm_env_vars = {var:os.getenv(var) for var in SLURM_ENV_VARS}
+        logging.debug(slurm_env_vars)
+        nnodes = int(slurm_env_vars['SLURM_JOB_NUM_NODES'])
+        nprocs_per_node = int(slurm_env_vars['SLURM_CPUS_ON_NODE'])
+        nprocs = nnodes * nprocs_per_node
+
         logging.debug(f'\nComputing n^2 for all n in [1, ..., {N}] with {nprocs} proceess{(nprocs > 1) * "es"} across {nnodes} nodes\n')
-
-    with MPIPoolExecutor() as executor:
-        for res in executor.map(test_work, range(N), [N] * N):
-            print(res)
-        
-        # for i, res in enumerate(executor.starmap(pow, [(n, 2) for n in range(N)])):
-        #     if i % (N / 10) == 0:
-        #         logging.info(f'{i}^2\t=\t{res:e}')
+    
+    with MPIPool(comm_world) as pool:
+        if pool.is_manager():
+            for i, res in enumerate(pool.map(test_work, range(N))):
+                if i % (N / 10) == 0:
+                    logging.info(f'{i}^2\t=\t{res:e}')
--- vipdopt/utils.py
+++ vipdopt/utils.py
@@ -1,10 +1,15 @@
 """Module containing common functions used throughout the package."""
 
+from typing import TypeVar
 
 import numpy as np
 import numpy.typing as npt
 
-Number = int | float | complex
+Number = TypeVar('Number', int, float, complex)
+
+# Generic types for type hints
+T = TypeVar('T') 
+R = TypeVar('R')
 
 def sech(z: npt.ArrayLike | Number) -> npt.ArrayLike | Number:
     """Hyperbolic Secant."""
