1dc2eb59a8c7e1c3a2a61d85e2a4c7369b60d27e
Tyronitar <tytymc3@gmail.com>
2023-12-27 14:00:01 -0800

    Remove MPI Features
    * MPI job manager was deemed unnecessary
    * MPI stuff may be further explored in the future (see `mpi` branch)

--- vipdopt/mpi/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Sub package containing all MPI related code."""
-
-from vipdopt.mpi.pool import BrokenExecutorError, FileExecutor, FunctionExecutor
-
-__all__ = ['FileExecutor', 'FunctionExecutor', 'BrokenExecutorError']
